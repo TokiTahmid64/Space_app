d92efdae8f75410ff66d0090958bf66372e96d13
Tokivaiya64 <44304799+Tokivaiya64@users.noreply.github.com>
2020-10-02 23:52:03 -0700

    Unity vr project

---
+++ New Unity Project (3)/Assets/nasa/show_hode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show_hode : MonoBehaviour {
+	public GameObject info;
+	private bool show=false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void show_hide()
+	{
+		if(!show)
+		{
+			info.SetActive(true);
+			show=true;
+		}
+		else
+		{
+			info.SetActive(false);
+			show=false;
+		}
+	}
+}
---
+++ New Unity Project (3)/Assets/nasa/show_hode_2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show_hode_2 : MonoBehaviour {
+	public GameObject info;
+	private bool show=true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void show_hide()
+	{
+		if(!show)
+		{
+			info.SetActive(true);
+			show=true;
+		}
+		else
+		{
+			info.SetActive(false);
+			show=false;
+		}
+	}
+}
